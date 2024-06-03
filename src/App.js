09e479a61bd750328cb7fd4449b81e50b7fa9763
Eduardo Levy <elevypo@gmail.com>
2024-06-03 23:17:42 +0300

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import WelcomePage from "./pages/welcome";
+import PrivateRoute from "./components/authRoutes";
+import HomePage from "./pages/home";
+import GastosPage from "./pages/gastosmes";
+import PlanPage from "./pages/plan";
+import ResumenTarjetasPage from "./pages/resumentarjetas";
+import HistoriaPage from "./pages/historia";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path="/user/home" element={<HomePage />}></Route>
+          <Route path="/user/gastos" element={<GastosPage />}></Route>
+          <Route path="/user/plan" element={<PlanPage />}></Route>
+          <Route path="/user/tarjetas" element={<ResumenTarjetasPage />}></Route>
+          <Route path="/user/historia" element={<HistoriaPage />}></Route>
+        </Route>
+        <Route path="/" element={<WelcomePage />}></Route>
+      </Routes>
+
+    </Router>
   );
 }
 
