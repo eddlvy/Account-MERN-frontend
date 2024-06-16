9169a4f14455c753df0ab04e89f7956a1b2dd1d0
Eduardo Levy <elevypo@gmail.com>
2024-06-16 23:02:20 +0300

    June16 bug fixes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,40 +1,10 @@
-import React, { useEffect, useContext } from "react";
-import { Context } from "../context/context";
+import React from "react";
 import NavComponent from "../components/navcomponent";
 import IngresosComponent from "../components/ingresosComponent"
 import GastosComponent from "../components/gastosComponent";
-import axios from "axios";
 
 
 function HomePage() {
-  const { mes, setMes } = useContext(Context);
-  const { ingresos, setIngresos } = useContext(Context);
-  const { plan, setPlan } = useContext(Context);
-
-
-
-  // mes context
-  useEffect(() => {
-    fetch("https://account-app-2d28ea94e3bf.herokuapp.com/mes").then(res => res.json()).then((data) => {
-      setMes(data[0].mes)
-    }, (error) => {
-      console.log(error)
-    }
-    )
-  })
-
-  // ingresos context
-
-  useEffect(() => {
-    axios.get(`https://account-app-2d28ea94e3bf.herokuapp.com/ingresos/${mes}`).then(res => setIngresos(res.data.total)).catch(error => { console.log(error) })
-  }, [ingresos, mes, setIngresos]);
-
-
-  // // Plan context
-  useEffect(() => {
-    axios.get(`https://account-app-2d28ea94e3bf.herokuapp.com/plan`).then(res => setPlan(res.data[0])).catch(error => console.log(error), [plan, setPlan])
-  });
-
   return (
     <div className="container-main">
       <NavComponent></NavComponent>
--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { Context } from "../context/context";
+import axios from "axios";
 import LoginComponent from "../components/login";
 import './style/welcome.css';
 import img from '../assets/pexels-nietjuh-796602.jpg';
@@ -7,6 +9,30 @@ import img from '../assets/pexels-nietjuh-796602.jpg';
 
 function WelcomePage() {
   let date = new Date();
+  const { mes, setMes } = useContext(Context);
+  const { ingresos, setIngresos } = useContext(Context);
+  const { plan, setPlan } = useContext(Context);
+  // mes context
+  useEffect(() => {
+    fetch("https://account-app-2d28ea94e3bf.herokuapp.com/mes").then(res => res.json()).then((data) => {
+      setMes(data[0].mes)
+    }, (error) => {
+      console.log(error)
+    }
+    )
+  })
+
+  // ingresos context
+
+  useEffect(() => {
+    axios.get(`https://account-app-2d28ea94e3bf.herokuapp.com/ingresos/${mes}`).then(res => setIngresos(res.data.total)).catch(error => { console.log(error) })
+  }, [ingresos, mes, setIngresos]);
+
+
+  // // Plan context
+  useEffect(() => {
+    axios.get(`https://account-app-2d28ea94e3bf.herokuapp.com/plan`).then(res => setPlan(res.data[0])).catch(error => console.log(error), [plan, setPlan])
+  }, [plan, setPlan]);
   return (
 
     <div className="main-container">
